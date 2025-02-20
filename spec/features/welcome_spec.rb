ac1f72bbb11a575cc9ea3bc1af55789eaa8a40cd
Dana Howell <howelld115@gmail.com>
2025-02-20 10:31:37 -0700

    Welcome page established

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'welcome index', type: :feature do
+  describe ' as a user when I visit welcome_path' do
+    it 'displays' do
+      visit '/'
+      expect(page).to have_content("Welcome to Rori Studios")
+    end
+  end
+end
